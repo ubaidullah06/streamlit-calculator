fc5f8bf948ef76163e1f6bdc9d98d80f35478690
Ubaidullah <ubaidullahatta97@gmail.com>
2024-07-15 15:21:09 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,30 +1,33 @@
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 st.title("WELCOME TO UBAID'S CALCULATOR")
 st.header("A simple calculator performs basic Arithmatic Functions  ")
-
+  # Initialize result to None
 
 a = st.number_input("Enter 1st number ")
 b = st.number_input("Enter 2nd number ")
 operation = st.radio('Select an operation \U00002935 ', ['Add \U00002795', 'Subtract \U00002796', 'Multiply \U00002716', 'Divide \U0001F4C9'])
-
+x = None
 if operation == "Add":
     import addition
-    result = addition.add(a, b)
+    x = addition.add(a, b)
 if operation == "Subtract":
     import subtraction
-    result = subtraction.subtract(a , b)
+    x = subtraction.subtract(a , b)
 if operation == "Multiply":
     import multiplication
-    result = multiplication.multiply(a , b)
+    x = multiplication.multiply(a , b)
 if operation == "Divide":
     import division
-    result = division.divide(a , b)
+    x = division.divide(a , b)
     if b == 0:
        st.error("ZeroDivisionError")
+
 if st.button('Calculate \U0001F4BB'):
-   st.success(result)
+       st.success(f'Result: {x}')
+
 if st.checkbox('Perform another calculation \U0001F504'):
-   st.rerun()
+       st.rerun()
